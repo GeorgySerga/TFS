07051b448dfad89514a4fd0b386ece44e288a8f3
GeorgySerga <georgy.serga@gmail.com>
2017-03-07 23:54:41 +0300

    Remove hash-table specific checks for task 1.1

--- hw1/1.1.js
+++ hw1/1.1.js
@@ -1,18 +1,9 @@
 function check() {
     var firstString = prompt('Enter the first string').toLowerCase();
     var secondString = prompt('Enter the second string').toLowerCase();
-    var marker = false;
-
-    if (firstString.length !== secondString.length) {
-        marker = true;
-    }
 
     var sortedFirstString = firstString.split('').sort().join('');
     var sortedSecondString = secondString.split('').sort().join('');
 
-    if (sortedFirstString !== sortedSecondString) {
-        marker = true;
-    }
-
-    marker ? alert('No') : alert('Yes');
+    (sortedFirstString === sortedSecondString) ? alert('Yes') : alert('No');
 }
