77d7e6a4eb37a586257b0eaaa065e825daee0d04
GeorgySerga <georgy.serga@gmail.com>
2017-03-07 23:46:36 +0300

    Refactor hw1 task 1.1 to use arrays, ternary if and make check case insensitive

--- hw1/1.1.js
+++ hw1/1.1.js
@@ -1,28 +1,18 @@
 function check() {
-    var firstString = prompt('Enter the first string');
-    var secondString = prompt('Enter the second string');
+    var firstString = prompt('Enter the first string').toLowerCase();
+    var secondString = prompt('Enter the second string').toLowerCase();
     var marker = false;
 
     if (firstString.length !== secondString.length) {
-    marker = true;
-    }
-
-    var charsCount = {};
-    for (var i = 0; i < firstString.length; i++) {
-    charsCount[firstString[i]] = charsCount[firstString[i]] !== undefined ? charsCount[firstString[i]] + 1 : 1;
-    charsCount[secondString[i]] = charsCount[secondString[i]] !== undefined ? charsCount[secondString[i]] - 1 : -1;
-    }
-
-    for (var char in charsCount) {
-        if (charsCount[char] !== 0) {
         marker = true;
-        break;
-    }
     }
 
-    if (marker) {
-    alert('No');
-    } else {
-    alert('Yes');
+    var sortedFirstString = firstString.split('').sort().join('');
+    var sortedSecondString = secondString.split('').sort().join('');
+
+    if (sortedFirstString !== sortedSecondString) {
+        marker = true;
     }
+
+    marker ? alert('No') : alert('Yes');
 }
